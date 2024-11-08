bc3bee29e33ea43a37f8e412773d505c63fdcee9
luchiniatwork <tiago@luchini.nyc>
2024-11-08 09:52:44 -0500

    Add agent-name-finder to make it more flexible for different routes

--- src/boulder/ring_adapter.clj
+++ src/boulder/ring_adapter.clj
@@ -1,27 +1,31 @@
 (ns boulder.ring-adapter
   (:require [jsonista.core :as json]))
 
-(defn create-simple-handler [agent-fetcher]
-  (fn [{:keys [request-method body-params query-params headers]
-        :as request}]
-    (if (and (= "application/json" (get headers "accept"))
-             (= :post request-method))
-      (let [{:keys [agent]} query-params]
-        (if-let [agent-proper (agent-fetcher agent)]
-          (try
-            {:status 200
-             :headers {"Content-Type" "application/json"}
-             :body (-> body-params agent-proper json/write-value-as-string)}
-            (catch Throwable ex
-              {:status 500
-               :headers {"Content-Type" "text/html"}
-               :body (.getMessage ex)}))
-          {:status 404
-           :headers {"Content-Type" "text/html"}
-           :body "not found"}))
-      {:status 400
-       :headers {"Content-Type" "text/html"}
-       :body "Only POST and application/json as an accept header are accepted"})))
+(defn create-simple-handler
+  ([agent-fetcher]
+   (create-simple-handler agent-fetcher (constantly nil)))
+  ([agent-fetcher agent-name-finder]
+   (fn [{:keys [request-method body-params query-params headers]
+         :as request}]
+     (if (and (= "application/json" (get headers "accept"))
+              (= :post request-method))
+       (let [agent (or (agent-name-finder request)
+                       (:agent query-params))]
+         (if-let [agent-proper (agent-fetcher agent)]
+           (try
+             {:status 200
+              :headers {"Content-Type" "application/json"}
+              :body (-> body-params agent-proper json/write-value-as-string)}
+             (catch Throwable ex
+               {:status 500
+                :headers {"Content-Type" "text/html"}
+                :body (.getMessage ex)}))
+           {:status 404
+            :headers {"Content-Type" "text/html"}
+            :body "not found"}))
+       {:status 400
+        :headers {"Content-Type" "text/html"}
+        :body "Only POST and application/json as an accept header are accepted"}))))
 
 (comment
   (let [agents {"a" identity
